c31536b07221ee38cff70e7393ff7bb6e9b06c21
Yogesh Kumar <yksainimbd@gmail.com>
2024-08-22 22:14:13 +0530

    schemas completed

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+// routes
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+
 export { app };
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,10 @@
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new apiResponse(200, "OK", "Health check passed"));
+});
+
+export { healthcheck };
